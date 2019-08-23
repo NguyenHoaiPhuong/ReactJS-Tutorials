6f9a1f332cf95043bb71914290ce76e98ea05ad4
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-08-23 19:18:37 +0700

    React fragment

---
+++ 12_fragment/src/app.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>Heading 1</h1>
+                <p>Contents</p>
+            </Fragment>
+        )
+    }
+}
+
+export default App
