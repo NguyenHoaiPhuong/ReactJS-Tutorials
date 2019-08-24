dcede4a68095dd1a7ab448d399df199bbb41e155
akagi <hoaiphuong.nguyen.vn@gmail.com>
2019-08-24 16:36:20 +0700

    Bootstrap components

---
+++ 30_bootstrap/src/components/Product.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Product.css'
+
+export class Product extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Product
