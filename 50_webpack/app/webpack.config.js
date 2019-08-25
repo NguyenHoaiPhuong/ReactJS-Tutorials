bcf1d4b3b7deef1995af6d4ac5872a2c41d3a424
NguyenHoaiPhuong <NguyenHoaiPhuong@github.com>
2019-08-25 22:25:43 +0700

    webpack intro

--- 50_webpack/app/webpack.config.js
+++ 50_webpack/app/webpack.config.js
@@ -1,21 +1,20 @@
-const path = require(‘path’);
-const HWP = require(‘html-webpack-plugin’);
+var path    = require('path');
+var hwp     = require('html-webpack-plugin');
+
 module.exports = {
-    entry: path.join(__dirname, ‘/src/index.js’),
+    entry: path.join(__dirname, '/src/index.js'),
     output: {
-        filename: ‘build.js’,
-        path: path.join(__dirname, ‘/dist’)
+        filename: 'build.js',
+        path: path.join(__dirname, '/dist')
     },
     module:{
         rules:[{
-            test: /\.js$/,
             exclude: /node_modules/,
-            loader: ‘babel-loader’
+            test: /\.js$/,
+            loader: 'babel-loader'
         }]
     },
     plugins:[
-        new HWP(
-            {template: path.join(__dirname,‘/src/index.html’)}
-        )
+        new hwp({template:path.join(__dirname, '/src/index.html')})
     ]
 }
