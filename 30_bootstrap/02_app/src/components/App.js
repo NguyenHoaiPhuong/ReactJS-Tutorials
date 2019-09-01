99c8bbc557835043f284f71d0d5aa7379832b128
NguyenHoaiPhuong <NguyenHoaiPhuong@github.com>
2019-09-02 00:24:25 +0700

    Props, event, ref

--- 30_bootstrap/02_app/src/components/App.js
+++ 30_bootstrap/02_app/src/components/App.js
@@ -4,24 +4,24 @@ import './style.css'
 import Course from './Course';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            reactLessons: ["Lesson 1", "Lesson 2", "Lesson 3"],
+            angularLessons: ["Lesson 1", "Lesson 2", "Lesson 5"],
+            golangLessons: ["Lesson 1", "Lesson 2", "Lesson 10"],
+            pythonLessons: ["Lesson 1", "Lesson 2", "Lesson 6"]
+        }
+    }
+    
     render() {
         return (
-            <div className="row">
-                <div className="col-sm-4">
-                    <Course name="ReactJS" time="40h" free={true} />               
-                </div>
-
-                <div className="col-sm-4">
-                    <Course name="Angular" time="50h" free={false} />              
-                </div>
-
-                <div className="col-sm-4">
-                    <Course name="Golang" time="100h" free={false} />
-                </div>
-
-                <div className="col-sm-4">
-                    <Course name="Python" time="80h" free={true} />
-                </div>
+            <div className="App row">                
+                <Course name="ReactJS" time="40h" content="Reactjs for beginners" free={true} lessons={this.state.reactLessons} />
+                <Course name="Angular" time="50h" content="Angular for beginners" free={false} lessons={this.state.angularLessons} />
+                <Course name="Golang" time="100h" content="Golang for beginners" free={false} lessons={this.state.golangLessons} />
+                <Course name="Python" time="80h" content="Python for beginners" free={true} lessons={this.state.pythonLessons} />
             </div>
         )
     }
--- 30_bootstrap/02_app/src/components/Course.js
+++ 30_bootstrap/02_app/src/components/Course.js
@@ -2,25 +2,56 @@ import React, { Component } from 'react'
 import Lession from './Lession'
 
 class Course extends Component {
-    render() {
+    showFreeBtn = () => {
+        let free = this.props.free
+        if (!free) {
+            return(
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="basic-addon1" onClick={this.register}>Register</span>
+                    </div>
+                    <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" ref="username" />
+                </div>
+            )
+        }
         return (
-            <div className="card" style={{width: '25rem'}}>
-                <div className="card-header" style={{backgroundColor: 'lightblue'}}>
-                    {this.props.name}
-                </div>
-                <div className="card-body">
-                    {this.props.time}
-                    {this.props.content}
-                    <ul className="list-group">
-                        <Lession name="Chapter 1" />
-                        <Lession name="Chapter 2" />
-                        <Lession name="Chapter 3" />
-                    </ul>                     
-                </div>
-                <div class="card-footer text-muted">
-                    2 days ago
-                </div>
+            <div className="btn-group" role="group" aria-label="Basic example">
+                <button type="button" className="btn btn-success">View 1</button>
+                <button type="button" className="btn btn-danger">View 2</button>
+                <button type="button" className="btn btn-secondary">View 3</button>
             </div>
+        )
+    }
+
+    register = () => {
+        alert(this.refs.username.value)
+    }
+
+    render() {
+        let {name, time, content, lessons} = this.props
+        const lessonComponents = lessons.map(lesson =>{
+            return(
+                <Lession key={lesson} name={lesson} />
+            )
+        })
+        return (
+            <div className="col-sm-4">
+                <div className="card" style={{width: '25rem'}}>
+                    <div className="card-header" style={{backgroundColor: 'lightblue'}}>
+                        {name}
+                    </div>
+                    <div className="card-body">
+                        <p>{time}</p>
+                        <p>{content}</p>
+                        <ul className="list-group">                            
+                            {lessonComponents}
+                        </ul>
+                    </div>
+                    <div className="card-footer text-muted">
+                        {this.showFreeBtn()}
+                    </div>
+                </div>
+            </div>            
         )
     }
 }
