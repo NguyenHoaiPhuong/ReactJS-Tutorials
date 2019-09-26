8ecc035d49fbee2325dbd03f5d162f3beb9bf012
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-09-26 14:48:20 +0700

    Rename folder

---
+++ 40_redux/01_tut/dev/js/reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux';
+import UserReducer from './users';
+import ActiveUserReducer from './active-user'
+
+const allReducers = combineReducers({
+    users: UserReducer,
+    activeUser: ActiveUserReducer
+})
+
+export default allReducers;
